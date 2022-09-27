12249741d6788b6ec2fb4e47f18496764b52c0b9
Nick <99430009+ntg00d@users.noreply.github.com>
2022-09-27 10:58:27 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Уйти от React Query
-// Сделать новую реализацию хуков
 // Изменить Tailwind
 
 root.render(<App />);
